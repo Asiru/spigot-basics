ecfb07e29732cb39ec01b609a758530bf5cbea84
Asiru <12991517+Asiru@users.noreply.github.com>
2020-01-31 15:55:06 -0500

    Added CooldownManager framework

---
+++ java/com/neo/basics/cooldown/CooldownManager.java
@@ -0,0 +1,52 @@
+package com.neo.basics.cooldown;
+
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class CooldownManager<T> {
+	private Map<T, Cooldown> cooldowns;
+	
+	public CooldownManager() {
+		this.cooldowns = new HashMap<>();
+	}
+	
+	public final long getCooldown(T object) {
+		if(cooldowns.containsKey(object))
+			return cooldowns.get(object).cooldown;
+		return 0;
+	}
+	
+	public final void setCooldown(T object, long cooldown) {
+		Cooldown c = cooldowns.get(object);
+		if(c == null) {
+			c = new Cooldown(object, cooldown);
+			cooldowns.put(object, c);
+			start(c);
+		} else {
+			c.cooldown = cooldown;
+		}
+	}
+	
+	protected abstract void start(Cooldown cooldown);
+	
+	private class Cooldown extends BukkitRunnable {
+		private T object;
+		private long cooldown;
+		
+		private Cooldown(T object, long cooldown) {
+			this.object = object;
+			this.cooldown = cooldown;
+		}
+		
+		@Override
+		public void run() {
+			cooldown--;
+			if(cooldown <= 0) {
+				cancel();
+				cooldowns.remove(object);
+			}
+		}
+	}
+}
